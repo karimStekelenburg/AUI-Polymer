cb47cbf3fddb070eccb48715524aa34b7b4a76d6
Karim Stekelenburg <karim.stekelenburg@me.com>
2017-04-16 14:31:36 +0200

    - lil' json-response fix in get_fietsen

--- Polymer3.1/python_cgi/get_fietsen.py
+++ Polymer3.1/python_cgi/get_fietsen.py
@@ -5,10 +5,8 @@ def getFietsen():
 		db.cursor.execute("SELECT * FROM FIETSEN")
 		db.cnx.commit()
 		fietsen = db.cursor.fetchall()
-		print(fietsen)
 		return fietsen
 
 db = sqlHandler.DbInterface(dbConfig.db)
-response = {"errorCode": getFietsen()}
-jsonHandler.serializeJson(response)
+jsonHandler.serializeJson(getFietsen())
 db.close()
