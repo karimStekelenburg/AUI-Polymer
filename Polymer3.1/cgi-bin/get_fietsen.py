32cd83a658e323779f7b12d1194ec140027c15a0
Dahir Warsame <dahirwarsame@gmail.com>
2017-04-18 12:49:34 +0200

    fixed get html links

--- Polymer3.1/cgi-bin/get_fietsen.py
+++ Polymer3.1/cgi-bin/get_fietsen.py
@@ -6,6 +6,10 @@ def getFietsen():
     db.cursor.execute("SELECT * FROM FIETSEN")
     db.cnx.commit()
     fietsen = db.cursor.fetchall()
+
+    for fiets in fietsen:
+        fiets['regDate'] = str(fiets['regDate'])
+
     return fietsen
 
 
